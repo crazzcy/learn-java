46d48fc372b783076236ed90a0a7fba00a6358ff
crazzcy <chenyang.me@outlook.com>
2020-09-24 10:03:52 +0800

    jvm class loader test

---
+++ jvm/src/main/java/org/chenyang/study/jvm/ConstClass.java
@@ -0,0 +1,7 @@
+package org.chenyang.study.jvm;/**
+  * @author : ChenYang
+  * @date : 2020-09-14 2:50 下午
+  * @since :
+  */
+public class ConstClass {
+}
---
+++ jvm/src/main/java/org/chenyang/study/jvm/SubClass.java
@@ -0,0 +1,7 @@
+package org.chenyang.study.jvm;/**
+  * @author : ChenYang
+  * @date : 2020-09-14 2:42 下午
+  * @since :
+  */
+public class SubClass {
+}
